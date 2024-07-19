d4823e562f03d889618a9a12e3073c66c9c4f424
Remana <sdharma721@gmail.com>
2024-07-19 15:49:13 +0700

    Update Member.php

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+    protected $table = 'members';
+    public $timestamps = true;
+    protected $fillable = [
+        'full_name',
+        'username',
+        'email',
+        'password',
+        'role',
+        'phone',
+        'created_at',
+        'updated_at',
+    ];
 }
