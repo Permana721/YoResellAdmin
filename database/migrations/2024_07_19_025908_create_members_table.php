e971be403629f252c18cec933452207f45f35cce
Remana <sdharma721@gmail.com>
2024-07-23 08:50:03 +0700

    Update 2024_07_19_025908_create_members_table.php

--- database/migrations/2024_07_19_025908_create_members_table.php
+++ database/migrations/2024_07_19_025908_create_members_table.php
@@ -15,19 +15,51 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('username',50)->nullable()->unique();
-            $table->string('email')->unique()->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->integer('phone');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->timestamp('password_changed_at')->nullable();
-            $table->rememberToken();
-            $table->biginteger('created_by');
-            $table->biginteger('updated_by');
-            $table->timestamps();
-        });
+            $table->string('username', 100);
+            $table->string('password', 150)->nullable();
+            $table->string('otp', 6)->nullable();
+            $table->string('full_name', 200)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->integer('zipcode_id')->nullable();
+            $table->string('phone1', 50)->nullable();
+            $table->string('phone2', 50)->nullable();
+            $table->string('email', 50)->nullable();
+            $table->integer('nric_type_id')->nullable();
+            $table->string('nric', 50)->nullable();
+            $table->string('store_code', 5)->nullable();
+            $table->integer('total_points')->default('0')->nullable();
+            $table->integer('total_undians')->default('0')->nullable();
+            $table->date('registered_at')->default('2016-01-01')->nullable();
+            $table->date('first_logged_in_at')->default('2016-01-01')->nullable();
+            $table->integer('question_1')->nullable();
+            $table->integer('question_2')->nullable();
+            $table->string('answer_1', 255)->nullable();
+            $table->string('answer_2', 255)->nullable();
+            $table->string('is_blocked', 5)->default('0')->nullable();
+            $table->integer('created_by')->default('1')->nullable();
+            $table->integer('updated_by')->default('1')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->string('favourite_stores')->nullable();
+            $table->string('store_site_code_sales')->nullable();
+            $table->string('password_md5')->nullable();
+            $table->integer('total_tokens')->default('0')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->smallInteger('approve_cso')->default('0');
+            $table->smallInteger('approve_admin')->default('0');
+            $table->timestamp('approve_cso_at')->default(DB::raw('now()'))->nullable();
+            $table->timestamp('approve_admin_at')->default(DB::raw('now()'))->nullable();
+            $table->smallInteger('approve_cso_by')->nullable();
+            $table->smallInteger('approve_admin_by')->nullable();
+            $table->string('tokopedia')->nullable();
+            $table->string('shopee')->nullable();
+            $table->string('bukalapak')->nullable();
+            $table->string('lain-lain')->nullable();
+            $table->string('remember_token')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('type_costumer', 100)->nullable();
+            $table->string('brand', 200)->nullable();
+        });        
     }
 
     /**
