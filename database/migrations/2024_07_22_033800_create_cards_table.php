3044a7535c5583e16e7d29f72755cb3ebe48b9e3
Remana <sdharma721@gmail.com>
2024-07-23 08:50:08 +0700

    Create 2024_07_22_033800_create_cards_table.php

---
+++ database/migrations/2024_07_22_033800_create_cards_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->integer('member_id')->nullable(false);
+            $table->string('number', 16)->unique()->nullable(false);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
