60cdc7bef51add5acfe20c59cf864b03aa010a70
Remana <sdharma721@gmail.com>
2024-07-31 11:35:28 +0700

    Update MenuController.php

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\User;
 use App\Models\Member;
 use Illuminate\Http\Request;
 use App\Http\Requests\MemberRequest;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Events\Verified;
 use RealRashid\SweetAlert\Facades\Alert;
+use Yajra\DataTables\DataTables;
 
 class MenuController extends Controller
 {
@@ -27,22 +29,6 @@ class MenuController extends Controller
         return view('menu.menu',[
             'data'  => $data,
             'title' => 'Menu list',
-        ]);
-    }
-
-    public function role(Request $request)
-    {
-        if ($request->ajax()) {
-            $data = Member::latest()->get();
-            return Datatables::of($data)
-                ->addIndexColumn()
-                ->make(true);
-        }
-
-        $data = Member::all();
-        return view('menu.role',[
-            'data'  => $data,
-            'title' => 'Role list',
         ]);
     }
 
