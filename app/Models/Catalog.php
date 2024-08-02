e354ede35978abb3f65cd312d5b7327839715bc4
Remana <sdharma721@gmail.com>
2024-08-02 10:23:23 +0700

    Create Catalog.php

---
+++ app/Models/Catalog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Catalog extends Model
+{
+    use HasFactory;
+    protected $table = 'catalogs';
+    public $timestamps = true;
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by',
+        'created_at',
+        'updated_at',
+        'whatsapp',
+        'url_catalog',
+        'store_code',
+    ];
+}
