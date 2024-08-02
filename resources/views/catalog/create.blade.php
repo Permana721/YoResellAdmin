d505b57d30114ea4a43a0ed2a32a8fe536654c87
Remana <sdharma721@gmail.com>
2024-08-02 10:23:29 +0700

    Create create.blade.php

---
+++ resources/views/catalog/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layout.app')
+@section('title', 'Add Catalog')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h4 class="card-title">Add Catalog</h4>
+    </div>
+    <div class="card-body">
+        <form method="post" action="{{ route('add.data.catalog') }}" id="frmSearch" class="invoice-repeater">
+        @csrf
+            <div class="row">
+                <div class="col-xl-6 col-md-6 col-12">
+                    <div class="form-group">
+                        <label for="name"><strong>Name:</strong></label>
+                        <input type="text" name="name" id="name" placeholder="name" class="form-control" autocomplete="off" required />
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6 col-12">
+                    <div class="form-group">
+                        <label for="whatsapp"><strong>Whatsapp (Gunakan awalan 62):</strong></label>
+                        <input type="text" name="whatsapp" id="whatsapp" placeholder="WhatsApp" class="form-control" autocomplete="off" required />
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6 col-12">
+                    <div class="form-group">
+                        <label for="url_catalog"><strong>URL Catalog:</strong></label>
+                        <input type="text" name="url_catalog" id="url_catalog" placeholder="Url Catalog" class="form-control" autocomplete="off" required />
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6 col-12">
+                    <div class="form-group">
+                        <label for="store_code"><strong>Store:</strong></label>
+                        <input type="text" name="store_code" id="store_code" placeholder="Store" class="form-control" autocomplete="off" required />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <a href="{{ route('catalog') }}" class="btn btn-info" id="backButton">Back</a>
+                <button type="submit" class="btn btn-primary" id="saveButton">Save</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
