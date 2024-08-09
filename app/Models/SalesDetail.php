8c7117a7cccd195bb1c4e52f32f6f98c57830e6e
Remana <sdharma721@gmail.com>
2024-08-09 15:54:56 +0700

    Create SalesDetail.php

---
+++ app/Models/SalesDetail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SalesDetail extends Model
+{
+    use HasFactory;
+    protected $table = 'sales_details';
+    public $timestamps = true;
+    protected $fillable = [
+        'tanggal',
+        'store_code',
+        'pos',
+        'trans',
+        'plu',
+        'description',
+        'gross',
+        'disc',
+        'qty',
+        'file_name',
+        'created_at',
+        'price',
+        'sku',
+        'sv',
+    ];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class, 'store_code', 'store_code');
+    }
+
+    public function masterArticle()
+    {
+        return $this->belongsTo(MasterArticle::class, 'plu', 'article_code');
+    }
+
+    public function salesHeader()
+    {
+        return $this->belongsTo(SalesHeader::class, 'store_code', 'store_code');
+    }
+
+}
