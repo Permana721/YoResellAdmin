b6c6580e40ef980e3534bfddefbccf23534ab425
Remana <sdharma721@gmail.com>
2024-08-16 14:59:29 +0700

    Update notfound.blade.php

--- resources/views/errors/notfound.blade.php
+++ resources/views/errors/notfound.blade.php
@@ -9,7 +9,7 @@
     <title>Error - 404 Not Found</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
-    <!-- BEGIN: Theme CSS-->
+    <!-- BEGIN:: Theme CSS-->
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.min.css') }}">
