e2f32efb3e998e4745e2fe2e5e2a00118e8a89b0
Remana <sdharma721@gmail.com>
2024-08-20 14:40:08 +0700

    Update index.blade.php

--- resources/views/transaction-member/index.blade.php
+++ resources/views/transaction-member/index.blade.php
@@ -7,6 +7,17 @@
 
 <div class="card">
     <div class="card-body">
+        <div class="row mb-2" style="position: relative; top: 5px;"> 
+            <div class="col-md-3">
+                <input type="date" id="fromDate" class="form-control" placeholder="From">
+            </div>
+            <div class="col-md-3">
+                <input type="date" id="toDate" class="form-control" placeholder="To">
+            </div>
+            <div class="col-md-2">
+                <button id="filter" class="btn btn-primary">Show</button>
+            </div>
+        </div>
         <section id="table-roles">
         <div class="row">
             <div class="col-12">
@@ -32,59 +43,70 @@
 
 @section('scripts')
 <script type="text/javascript">
+    $('#filter').on('click', function() {
+        $('#detailedTable').DataTable().ajax.reload();
+    });
+
     $(document).ready(function() {
-        let dtdom = '<"card-header border-bottom p-1"<"head-label"><"dt-action-buttons text-right"B>><"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>';
-
-        $('#detailedTable').DataTable({
-    processing: true,
-    serverSide: true,
-    ajax: "{{ route('transaction.getTransactionMember') }}",
-    columns: [
-        { data: 'store', name: 'store.name' },
-        { data: 'name', name: 'full_name' },
-        { data: 'member', name: 'card.number' },
-        { data: 'omset_qty', name: 'omset_qty', searchable: false, orderable: false },
-        { data: 'omset_rupiah', name: 'omset_rupiah', searchable: false, orderable: false },
-    ],
-    dom: dtdom,
-    lengthMenu: [
-        [10, 25, 50, 75, 100],
-        ['10', '25', '50', '75', '100']
-    ],
-    buttons: [
-        {
-            extend: 'copy',
-            text: 'Copy',
-            className: 'btn btn-primary'
-        },
-        {
-            extend: 'excel',
-            text: 'Excel',
-            className: 'btn btn-primary'
-        },
-        {
-            extend: 'csv',
-            text: 'CSV',
-            className: 'btn btn-primary'
-        },
-        {
-            extend: 'pdf',
-            text: 'PDF',
-            className: 'btn btn-primary'
-        }
-    ],
-    language: {
-        paginate: {
-            previous: '&nbsp;',
-            next: '&nbsp;'
-        },
-        info: "Showing _START_ to _END_ of _TOTAL_ entries"
-    },
-    scrollX: true
-});
-
-$('div.head-label').html('<h6 class="mb-0">Transaction Member Detail</h6>');
-
+        var table = $('#detailedTable').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: "{{ route('transaction.getTransactionMember') }}",
+                data: function(d) {
+                    d.fromDate = $('#fromDate').val();
+                    d.toDate = $('#toDate').val();
+                }
+            },
+            columns: [
+                { data: 'store', name: 'store.name' },
+                { data: 'name', name: 'full_name' },
+                { data: 'member', name: 'card.number' },
+                { data: 'omset_qty', name: 'omset_qty', searchable: false, orderable: false },
+                { data: 'omset_rupiah', name: 'omset_rupiah', searchable: false, orderable: false },
+            ],
+            dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-md-6"B><"col-md-6"f>>' + 
+                't' +
+                '<"row"<"col-md-6"i><"col-md-6"p>>',
+            lengthMenu: [
+                [10, 25, 50, 75, 100],
+                ['10', '25', '50', '75', '100']
+            ],
+            buttons: [
+                {
+                    extend: 'copy',
+                    text: 'Copy',
+                    className: 'btn btn-primary'
+                },
+                {
+                    extend: 'excel',
+                    text: 'Excel',
+                    className: 'btn btn-primary'
+                },
+                {
+                    extend: 'csv',
+                    text: 'CSV',
+                    className: 'btn btn-primary'
+                },
+                {
+                    extend: 'pdf',
+                    text: 'PDF',
+                    className: 'btn btn-primary'
+                }
+            ],
+            language: {
+                paginate: {
+                    previous: '&nbsp;',
+                    next: '&nbsp;'
+                },
+                info: "Showing _START_ to _END_ of _TOTAL_ entries"
+            },
+            scrollX: true
+        });
+        
+        $('#filter').on('click', function() {
+            table.ajax.reload();
+        });
     });
 
     $.ajaxSetup({
