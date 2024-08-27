4aed89ef0b91f8b2d13eac879a421b142b6db66d
Remana <sdharma721@gmail.com>
2024-08-27 14:09:05 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
     <meta name="description" content="YoPule.">
     <meta name="author" content="Raey Evolution">
-    <title>Login | YoResellAdmin</title>
+    <title>User Login | YoResellAdmin</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('content/uploads/favicon.png') }}">
     <link href="{{ asset('app-assets/css/google-font/google-fonts.css') }}" rel="stylesheet">
     <!-- BEGIN: Vendor CSS-->
