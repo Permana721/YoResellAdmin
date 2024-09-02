17b72c10801aba9d594f31593e089f30592ed724
Remana <sdharma721@gmail.com>
2024-09-02 15:18:28 +0700

    Update TransactionMemberController.php

--- app/Http/Controllers/TransactionMemberController.php
+++ app/Http/Controllers/TransactionMemberController.php
@@ -21,8 +21,26 @@ class TransactionMemberController extends Controller
 
     public function getTransaction(Request $request)
     {
-        $members = Member::with(['store', 'card', 'store.salesDetail'])
+        $user = auth()->user();
+        $roleId = $user->role_id;
+        $userStoreCode = $user->store_code;
+
+        $members = Member::with(['store', 'card', 'card.salesHeaders.salesDetails'])
             ->select('members.*');
+
+        if ($roleId == 3 && $userStoreCode) {
+            $members->whereHas('store', function ($query) use ($userStoreCode) {
+                $query->where('store_code', $userStoreCode);
+            });
+        }
+
+        if ($request->has('fromDate') && $request->has('toDate')) {
+            $fromDate = $request->input('fromDate');
+            $toDate = $request->input('toDate');
+            $members->whereHas('card.salesHeaders.salesDetails', function ($query) use ($fromDate, $toDate) {
+                $query->whereBetween('tanggal', [$fromDate, $toDate]);
+            });
+        }
 
         return DataTables::of($members)
             ->addColumn('store', function ($member) {
@@ -34,15 +52,48 @@ class TransactionMemberController extends Controller
             ->addColumn('member', function ($member) {
                 return $member->card ? $member->card->number : 'N/A';
             })
-            ->addColumn('omset_qty', function ($member) {
-                $totalQty = $member->store->salesDetail->sum('qty');
-                return $totalQty ? $totalQty : 0;
+            ->addColumn('omset_qty', function ($member) use ($request) {
+                $fromDate = $request->input('fromDate');
+                $toDate = $request->input('toDate');
+
+                if ($member->card) {
+                    $query = $member->card->salesHeaders->flatMap(function ($header) {
+                        return $header->salesDetails;
+                    });
+
+                    if ($fromDate && $toDate) {
+                        $query = $query->filter(function ($detail) use ($fromDate, $toDate) {
+                            return $detail->tanggal >= $fromDate && $detail->tanggal <= $toDate;
+                        });
+                    }
+
+                    $totalQty = $query->sum('qty');
+                    return $totalQty;
+                }
+                return 0;
             })
-            ->addColumn('omset_rupiah', function ($member) {
-                $totalRupiah = $member->store->salesDetail->sum(function ($detail) {
-                    return $detail->qty * $detail->price;
-                });
-                return $totalRupiah ? number_format($totalRupiah) : 0;
+            ->addColumn('omset_rupiah', function ($member) use ($request) {
+                $fromDate = $request->input('fromDate');
+                $toDate = $request->input('toDate');
+
+                if ($member->card) {
+                    $query = $member->card->salesHeaders->flatMap(function ($header) {
+                        return $header->salesDetails;
+                    });
+
+                    if ($fromDate && $toDate) {
+                        $query = $query->filter(function ($detail) use ($fromDate, $toDate) {
+                            return $detail->tanggal >= $fromDate && $detail->tanggal <= $toDate;
+                        });
+                    }
+
+                    $totalRupiah = $query->sum(function ($detail) {
+                        return $detail->gross - $detail->disc;
+                    });
+
+                    return $totalRupiah ? number_format($totalRupiah) : 0;
+                }
+                return 0;
             })
             ->make(true);
     }
